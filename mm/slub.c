711b29894078eaf7df2f1a1ab6a15995feb32bd4
Tobin C. Harding <tobin@kernel.org>
2019-01-26 17:22:01 +1100

    slub: Add defrag_used_ratio field and sysfs support
    In preparation for enabling defragmentation of slab pages.
    
    "defrag_used_ratio" is used to set the threshold at which
    defragmentation should be attempted on a slab page.
    
    "defrag_used_ratio" is a percentage in the range of 0 - 100 (inclusive).
    If less than that percentage of slots in a slab page are in use then the
    slab page will become subject to defragmentation.
    
    Add a defrag ratio field and set it to 30% by default. A limit of 30%
    specifies that more than 3 out of 10 available slots for objects need to
    be in use otherwise slab defragmentation will be attempted on the
    remaining objects.
    
    Co-Developed-by: Christoph Lameter <cl@linux.com>
    Signed-off-by: Tobin C. Harding <tobin@kernel.org>

--- mm/slub.c
+++ mm/slub.c
@@ -3623,6 +3623,7 @@ static int kmem_cache_open(struct kmem_cache *s, slab_flags_t flags)
 
 	set_cpu_partial(s);
 
+	s->defrag_used_ratio = 30;
 #ifdef CONFIG_NUMA
 	s->remote_node_defrag_ratio = 1000;
 #endif
@@ -5109,6 +5110,28 @@ static ssize_t destroy_by_rcu_show(struct kmem_cache *s, char *buf)
 }
 SLAB_ATTR_RO(destroy_by_rcu);
 
+static ssize_t defrag_used_ratio_show(struct kmem_cache *s, char *buf)
+{
+	return sprintf(buf, "%d\n", s->defrag_used_ratio);
+}
+
+static ssize_t defrag_used_ratio_store(struct kmem_cache *s,
+					const char *buf, size_t length)
+{
+	unsigned long ratio;
+	int err;
+
+	err = kstrtoul(buf, 10, &ratio);
+	if (err)
+		return err;
+
+	if (ratio <= 100)
+		s->defrag_used_ratio = ratio;
+
+	return length;
+}
+SLAB_ATTR(defrag_used_ratio);
+
 #ifdef CONFIG_SLUB_DEBUG
 static ssize_t slabs_show(struct kmem_cache *s, char *buf)
 {
@@ -5433,6 +5456,7 @@ static struct attribute *slab_attrs[] = {
 	&validate_attr.attr,
 	&alloc_calls_attr.attr,
 	&free_calls_attr.attr,
+	&defrag_used_ratio_attr.attr,
 #endif
 #ifdef CONFIG_ZONE_DMA
 	&cache_dma_attr.attr,
